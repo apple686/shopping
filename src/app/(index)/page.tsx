cd3d76760db104f03b4033490028dfa923e1960c
twp <1427257988@qq.com>
2025-12-08 21:45:31 +0800

    feat: 小修改

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,7 +1,11 @@
+import Sort from "@/components/Sort";
+
 export default function Page() {
     return (
         <div className="flex-1">
-            page
+            <div className="container m-auto">
+                <Sort />
+            </div>
         </div>
     )
 }
---
+++ src/components/Sort.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function Sort() {
+    return (
+        <div className="py-10">
+            sort
+        </div>
+    )
+}
